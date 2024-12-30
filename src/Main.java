c5ddfd7e3916225e2c8ced3dd870fb091a61f2c7
Vel <avior@gmail.com>
2024-12-30 20:16:08 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -5,3 +5,9 @@ public class Main {
 }
 
 //git init - ініціалізація проекту
+//git remote add origin https://github.com/Aviory/hw33.git - добаоення удаленного серверу
+
+
+//git add .   - добавляти файли для слідкування гіта
+//git commit -m "first commit" - це сейв, на компьютері
+//git push origin master
